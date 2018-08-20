d82b19385ba6176a64d2bfcb2a5424e86c2fd0a5
Ruposree <meerimi1258@gmail.com>
2018-08-21 01:04:00 +0530

    my third commit

--- src/org/practiceset1/testb/script/TestFbAutosuggestion.java
+++ src/org/practiceset1/testb/script/TestFbAutosuggestion.java
@@ -1,5 +1,13 @@
 package org.practiceset1.testb.script;
 
+import org.testng.annotations.Test;
+
 public class TestFbAutosuggestion {
+	
+	@Test
+	public static void Test() {
+		System.out.println("Inside autosuggestion");
+	}
+	
 
 }
