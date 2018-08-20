b1702fbbcbb01a9cb828c602c04b60dfafc1bc1e
Ruposree <meerimi1258@gmail.com>
2018-08-21 01:10:53 +0530

    my 4th commit

---
+++ src/org/practiceset1/testb/script/TestLink.java
@@ -0,0 +1,12 @@
+package org.practiceset1.testb.script;
+
+import org.testng.annotations.Test;
+
+public class TestLink {
+	@Test
+	public static void Test() {
+		System.out.println("Inside TestLink Class");
+	}
+	
+
+}
