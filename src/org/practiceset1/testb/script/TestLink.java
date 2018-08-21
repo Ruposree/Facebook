ed5038262cdaccb4235053886a3d253304068b83
Ruposree <meerimi1258@gmail.com>
2018-08-21 13:25:14 +0530

    my 9th commit

--- src/org/practiceset1/testb/script/TestLink.java
+++ src/org/practiceset1/testb/script/TestLink.java
@@ -1,11 +1,13 @@
 package org.practiceset1.testb.script;
 
+//mvn clean test -DsuiteXmlFile=src/test/resources/api/TestngSuite.xml
 import org.testng.annotations.Test;
 
 public class TestLink {
 	@Test
 	public static void Test() {
 		System.out.println("Inside TestLink Class");
+		System.out.println("Inside TestLink Class2");
 		System.out.println("Inside TestLink Class2");
 	
 		
